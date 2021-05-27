000791a6e4ea7eb83cdb4da94f0ebd410b60e1be
Kartik Yadav <kartikyadav94@gmail.com>
2021-05-28 01:39:16 +0530

    created Navbar

---
+++ frontend/src/Routes/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    position: relative;
+    margin-top: 70px;
+`;
+
+export const HomePage = () => {
+    return (
+        <Container>
+            HomePage
+        </Container>
+    )
+}
--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { Navbar } from "./Navbar";
 
 export default function Routes() {
-  return <div></div>;
+    return (
+        <div>
+            <Navbar />
+            <Switch>
+                <Route path='/' exact>
+                    <HomePage />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
