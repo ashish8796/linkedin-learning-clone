ace6db3f92717f9c219e73b3ddecd9df77ae6ab1
k.saikrishna <k.saikrishna78@gmail.com>
2021-05-30 19:58:55 +0530

    the main connection changes

--- server/dist/js/app.js
+++ server/dist/js/app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
@@ -10,25 +12,21 @@ const index_js_1 = __importDefault(require("./routes/index.js"));
 // const bodyParser = require('body-parser')
 const app = express_1.default();
 const PORT = process.env.PORT || 5000;
-const URI = "mongodb+srv://linkdenlearning:linkdenLearningDB@cluster0.ldxhc.mongodb.net/linkdenVideos?retryWrites=true&w=majority";
+const URI =
+  "mongodb+srv://linkdenlearning:linkdenLearningDB@cluster0.ldxhc.mongodb.net/linkdenVideos?retryWrites=true&w=majority";
 app.use(express_1.default.json());
 app.use(cors_1.default());
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(require('body-parser').urlencoded({ extended: false }));
-const uri = process.env.MONGODB_URI || `mongodb://localhost:27017/linkdenLearning`;
+const uri =
+  process.env.MONGODB_URI || `mongodb://localhost:27017/linkdenLearning`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose_1.default.set('useFindAndModify', false);
-<<<<<<< Updated upstream
-app.use(index_js_1.default);
-if (process.env.NODE_ENV == 'production') {
-    app.use(express_1.default.static('../../../frontend/build'));
-}
-mongoose_1.default.connect(URI, options).then(() => app.listen(PORT, () => console.log(`server lets see hosted on ${PORT}`)));
-=======
+mongoose_1.default.set("useFindAndModify", false);
 app.use(index_1.default);
-if (process.env.NODE_ENV == 'production') {
-    app.use(express_1.default.static('../../../frontend/build'));
+if (process.env.NODE_ENV == "production") {
+  app.use(express_1.default.static("../../../frontend/build"));
 }
-mongoose_1.default.connect(URI, options).then(() => app.listen(PORT, () => console.log(`server hosted on ${PORT}`)));
->>>>>>> Stashed changes
+mongoose_1.default
+  .connect(URI, options)
+  .then(() => app.listen(PORT, () => console.log(`server hosted on ${PORT}`)));
 // app.listen(3000,()=>console.log("connected"))
