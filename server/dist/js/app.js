42bd1caa17775d72325e70ddbb4d1d6babe4da09
k.saikrishna <k.saikrishna78@gmail.com>
2021-06-12 14:56:30 +0530

    certain changes in the keys on env

--- server/dist/js/app.js
+++ server/dist/js/app.js
@@ -7,27 +7,29 @@ const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 const index_1 = __importDefault(require("./routes/index"));
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express_1.default();
 // const fs= require('fs');
 const teachers = require("./utils/data/teachers");
 const PORT = process.env.PORT || 5000;
 // console.log(teachers)
 // const uris="mongodb+srv://vedansh:vedansh@coursera.03cjh.mongodb.net/coursera?retryWrites=true&w=majority"
-const URI = process.env.MONGODB_URI || "mongodb+srv://linkdenlearning:linkdenLearningDB@cluster0.ldxhc.mongodb.net/linkdenVideos?retryWrites=true&w=majority";
+const URI = process.env.MONGODB_URI || "";
 app.use(express_1.default.json());
 app.use(cors_1.default());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(require('body-parser').urlencoded({ extended: false }));
+app.use(require("body-parser").urlencoded({ extended: false }));
 // const uri:string = process.env.MONGODB_URI|| `mongodb://localhost:27017/linkdenLearning`
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose_1.default.set('useFindAndModify', false);
+mongoose_1.default.set("useFindAndModify", false);
 app.use(index_1.default);
-if (process.env.NODE_ENV == 'production') {
-    app.use(express_1.default.static('../../../frontend/build/index.html'));
+if (process.env.NODE_ENV == "production") {
+    app.use(express_1.default.static("../../../frontend/build/index.html"));
 }
 // console.log(teachers)
 // let file=fs.readFileSync("./utils/data/teachers");
 // console.log(file)
-mongoose_1.default.connect(URI, options).then(() => app.listen(PORT, () => console.log(`server lets see hosted on ${PORT}`)));
+mongoose_1.default
+    .connect(URI, options)
+    .then(() => app.listen(PORT, () => console.log(`server lets see hosted on ${PORT}`)));
 // app.listen(3000,()=>console.log("connected"))
