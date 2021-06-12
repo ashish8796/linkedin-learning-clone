b4a04fe84aac0db335f81d3adfe0e95f001be655
k.saikrishna <k.saikrishna78@gmail.com>
2021-06-12 13:01:38 +0530

    the security check for the details

--- server/src/controllers/utils/Index.ts
+++ server/src/controllers/utils/Index.ts
@@ -1,18 +1,21 @@
 import user from "../../models/user";
-import express from 'express'
+import express from "express";
 
-export const checkMailId = async ( req: express.Request, res: express.Response):Promise<void> =>{
-    try{
-        const {mail}= req.body;
-         const data=await user.find({emailId:mail})
+export const checkMailId = async (
+  req: express.Request,
+  res: express.Response
+): Promise<void> => {
+  try {
+    const { mail } = req.body;
+    const data = await user.find({ emailId: mail });
 
-         if(data.length==0){
-             res.status(200).send({message:"user doesn't exist"})
-            }
-            res.status(203).send({message:"user exist"})
-            res.end()
-    }catch(err){
-        console.log(err);
-        res.end()
+    if (data.length == 0) {
+      res.status(200).send({ message: "user doesn't exist" });
     }
-}
+    res.status(203).send({ message: "user exist" });
+    res.end();
+  } catch (err) {
+    console.log(err);
+    res.end();
+  }
+};
