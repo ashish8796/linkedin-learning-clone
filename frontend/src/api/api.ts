ea768adf59fff5e0ffc2a50ecfb22d1e53b976f7
Kartik Yadav <kartikyadav94@gmail.com>
2021-06-13 12:04:13 +0530

    added signin functionality

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,11 +1,22 @@
 import Axios from "axios";
+import { IRegister } from "../Components/Register/Register";
+import { ILogin } from '../Components/SignIn/SignIn';
 
 const axios = Axios.create({
-  headers: {
-    "Content-Type": "application/json"
-  }
+    baseURL: "http://localhost:5000/",
+    headers: {
+        "Content-Type": "application/json"
+    }
 })
 
 export const getData = () => {
-  return axios.get("/")
+    return axios.get("/")
+}
+
+export const registerUsers = (payload: IRegister) =>{
+    return axios.post('/register', payload)
+}
+
+export const loginUsers = (payload: ILogin) =>{
+    return axios.post('/login', payload)
 }
