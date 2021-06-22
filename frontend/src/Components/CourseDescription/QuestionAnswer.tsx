5f38351e3cead98d3f74d09e5024e2d47c206323
ashish8796 <ashishsaini.ak@gmail.com>
2021-06-22 22:52:38 +0530

    Changed some code in feature upload

--- frontend/src/Components/CourseDescription/QuestionAnswer.tsx
+++ frontend/src/Components/CourseDescription/QuestionAnswer.tsx
@@ -1,27 +1,26 @@
-import { Typography } from '@material-ui/core';
-import React from 'react'
+import { Typography } from "@material-ui/core";
+import React from "react";
 import styled from "styled-components";
-import Test from '../QuestionNAnswer/Test';
-export default function QuestionAnswer({id}:any) {
-    return (
-        <Content>
-            
-        <Test id={id}/>
-            
-        {/* <SideBar>
+import Test from "../QuestionNAnswer/Test";
+export default function QuestionAnswer({ id }: any) {
+  return (
+    <Content>
+      <Test id={id} />
+
+      {/* <SideBar>
         <Typography variant="h6">
                 Related Course
             </Typography> */}
-            {/* <div> */}
-                            {/* <Course>
+      {/* <div> */}
+      {/* <Course>
                                 <img  src="https://linkden-learning.s3.ap-south-1.amazonaws.com/course-thumbnails//1243db26-d136-4f54-bac9-c467c6ead24d_" alt="" />
                                 <CourseDetails2>
                                     <p>COURSE</p>
                                     <Typography variant="h6">Figure Drawing</Typography>
                                     <Typography variant="h5"> {"10,000"} Learns</Typography> */}
-                                    {/* <span>Progress</span> */}
-                                    {/* <span><LinearProgress variant="determinate" color={"primary"} value={15} /></span> */}
-                                {/* </CourseDetails2>
+      {/* <span>Progress</span> */}
+      {/* <span><LinearProgress variant="determinate" color={"primary"} value={15} /></span> */}
+      {/* </CourseDetails2>
                             </Course>
                             <Course>
                                 <img src="https://linkden-learning.s3.ap-south-1.amazonaws.com/course-thumbnails//f855d5f9-372f-4b13-9ce1-32684f2db4a0_" alt="" />
@@ -30,83 +29,80 @@ export default function QuestionAnswer({id}:any) {
                                     <Typography variant="h6">TITLE</Typography>
                                     <Typography variant="h5"> {"10,000"} Learns</Typography> */}
 
-                                    {/* <span><LinearProgress variant="determinate" value={10} /></span> */}
-                                {/* </CourseDetails2>
+      {/* <span><LinearProgress variant="determinate" value={10} /></span> */}
+      {/* </CourseDetails2>
                             </Course> */}
-                        {/* </div> */}
-                        {/* <div>
+      {/* </div> */}
+      {/* <div>
                             <span  style={{color:"#0073b1" , fontWeight:500}}>Show all</span>
                         </div> */}
-        {/* </SideBar> */}
+      {/* </SideBar> */}
     </Content>
-    )
+  );
 }
 
 const Content = styled.section`
-    display: grid;
-    width: 50rem;
-    margin: auto;
-    margin-top: 5px;
-    height:70rem;
-    /* grid-template-columns: 80% 20%; */
-`
+  display: grid;
+  width: 50rem;
+  margin: auto;
+  margin-top: 5px;
+  height: 70rem;
+  /* grid-template-columns: 80% 20%; */
+`;
 
 const SideBar = styled.aside`
-    border-left:0.2px solid black;
-    padding:15px
-`
-
+  border-left: 0.2px solid black;
+  padding: 15px;
+`;
 
 const CourseDetails = styled.div`
-        display: grid;
-        margin-top: 20px;
-        grid-template-rows: 50px 35px 25px auto;
-        height: auto;
-        h3{
-            font-size: 1.5em;
-            align-items: flex-start;
-        }
-        div{
-            display:grid;
-            /* flex-basis: 20%; */
-            grid-template-columns: 80px 102px 102px;
-            grid-column-gap: 10px;
-            span{
-                margin: 0%;
-                padding: 0%;
-            }
-        }
-
-`
+  display: grid;
+  margin-top: 20px;
+  grid-template-rows: 50px 35px 25px auto;
+  height: auto;
+  h3 {
+    font-size: 1.5em;
+    align-items: flex-start;
+  }
+  div {
+    display: grid;
+    /* flex-basis: 20%; */
+    grid-template-columns: 80px 102px 102px;
+    grid-column-gap: 10px;
+    span {
+      margin: 0%;
+      padding: 0%;
+    }
+  }
+`;
 
 const Course = styled.div`
-    display: grid;
-    grid-template-columns: 40% 60%;
-    padding-top: 2%;
-    padding-bottom: 2%;
-    img{
-        width:7rem
-    }
-`
+  display: grid;
+  grid-template-columns: 40% 60%;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  img {
+    width: 7rem;
+  }
+`;
 
 const CourseDetails2 = styled.div`
-    display: grid;
-    grid-template-rows: 1fr 2fr 2rem;
-    height: 50px;
-    line-height: 0.5em;
+  display: grid;
+  grid-template-rows: 1fr 2fr 2rem;
+  height: 50px;
+  line-height: 0.5em;
+  font-size: small;
+  /* padding: ; */
+  h5 {
     font-size: small;
-    /* padding: ; */
-    h5{
-        font-size: small;
-        padding-top: 5px;
-        padding-bottom: 5px;
-    }
-    p{
-        padding: 0%;
-        margin: 0%;
-    }
-    span{
-        padding:0%;
-    }
-    
-`
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+  p {
+    padding: 0%;
+    margin: 0%;
+  }
+  span {
+    padding: 0%;
+  }
+`;
--- frontend/src/Components/QuestionNAnswer/Test.tsx
+++ frontend/src/Components/QuestionNAnswer/Test.tsx
@@ -1,133 +1,134 @@
-import React from 'react'
-import QuestionNAnswer from './QuestionNAnswer'
+import React from "react";
+import QuestionNAnswer from "./QuestionNAnswer";
 import axios from "axios";
 import styled from "styled-components";
-import { useSelector } from 'react-redux';
-import { State } from '../../store/tsTypes';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { useSelector } from "react-redux";
+import { State } from "../../store/tsTypes";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
-
-export interface IComments{
-    courseId?: string;
+export interface IComments {
+  courseId?: string;
   question: string;
   userId: any;
   answers?: any;
-  _id:string
+  _id: string;
 }
-const Button= styled.button`
-    color: white;
-    background-color: #0073b1;
-    padding: 0.5% 15px;
-`
-const CommentBox=styled.div`
-    display: grid;
-    grid-template-rows: 50px auto 20px;
-    margin: auto;
-    width: 90%;
-    border: 1px solid black;
-    padding:1rem;
-    img{
-        height:40px;
-        border-radius: 50%;
-    }
-
-`
-const ImageHolder=styled.div`
+const Button = styled.button`
+  color: white;
+  background-color: #0073b1;
+  padding: 0.5% 15px;
+`;
+const CommentBox = styled.div`
+  display: grid;
+  grid-template-rows: 50px auto 20px;
+  margin: auto;
+  width: 90%;
+  border: 1px solid black;
+  padding: 1rem;
+  img {
+    height: 40px;
     border-radius: 50%;
-    height:50px;
-`
-const NameTag=styled.div`
-    display: grid;
-    grid-template-columns: 8% 79%;
-    
-`
+  }
+`;
+const ImageHolder = styled.div`
+  border-radius: 50%;
+  height: 50px;
+`;
+const NameTag = styled.div`
+  display: grid;
+  grid-template-columns: 8% 79%;
+`;
 
-const Box=styled.div`
-    textarea{
-        width:100%;
-        height: auto;
-        min-height: 100px;
-    }
-`
+const Box = styled.div`
+  textarea {
+    width: 100%;
+    height: auto;
+    min-height: 100px;
+  }
+`;
 
-export default function Test({id="60c6e5a4bac4a7241c74f84f"}:any) {
-    const [comments, setComments] = React.useState([])
-    const [message,setMessage]=React.useState("")
-    const [question,setQuestion]= React.useState("")
-    const userDetails = useSelector((state:State) => state.user.userDetails)
-    const {firstName , lastName,emailId} =userDetails
-    const handlePostQuestion = async ( )=>{
-        const  payload= {
-            "question":question,
-            "userId":userDetails._id,
-            "courseId":id ||"60c6e5a4bac4a7241c74f84f"
-        }
-         let response= await axios.post('http://localhost:5000/add-question',payload);
-         console.log(response)
-         setQuestion("")
-        // setMessage(response?.data.message) 
-    }
-    const calling = async()=>{
-        try {
-            
-            let data = await axios.get(`http://localhost:5000/getQnAWithCourseId/${id}`)    
-   
-            setComments(data.data.QNA)
-        
-        } catch (error) {
-            console.log(error)
-        }
+export default function Test({ id = "60c6e5a4bac4a7241c74f84f" }: any) {
+  const [comments, setComments] = React.useState([]);
+  const [message, setMessage] = React.useState("");
+  const [question, setQuestion] = React.useState("");
+  const userDetails = useSelector((state: State) => state.user.userDetails);
+  const { firstName, lastName, emailId } = userDetails;
+  const handlePostQuestion = async () => {
+    const payload = {
+      question: question,
+      userId: userDetails._id,
+      courseId: id || "60c6e5a4bac4a7241c74f84f",
+    };
+    let response = await axios.post(
+      "http://localhost:5000/add-question",
+      payload
+    );
+    console.log(response);
+    setQuestion("");
+    // setMessage(response?.data.message)
+  };
+  const calling = async () => {
+    try {
+      let data = await axios.get(
+        `http://localhost:5000/getQnAWithCourseId/${id}`
+      );
 
+      setComments(data.data.QNA);
+    } catch (error) {
+      console.log(error);
     }
-    React.useEffect(()=>{
-        calling()
-        // handlePostQuestion()
-    },[comments])
+  };
+  React.useEffect(() => {
+    calling();
+    // handlePostQuestion()
+  }, [comments]);
 
-    const userData = {
-        name:"name",
-        clg:"collage"
-    }
-    // calling()
-    return (
-        <section  style={{height:"auto"}}>
-            {/* <div>
+  const userData = {
+    name: "name",
+    clg: "collage",
+  };
+  // calling()
+  return (
+    <section style={{ height: "auto" }}>
+      {/* <div>
                 {message.length!==0 && <h1>{message}</h1>}
             </div> */}
-        <CommentBox>
-            
+      <CommentBox>
         <NameTag>
-                <ImageHolder>
-                    {/* <img src="https://via.placeholder.com/168x160" alt="" /> */}
-                    <AccountCircleIcon  style={{fontSize:"3em",color:"grey"}}/>
-                </ImageHolder>
-                <div>
-                    <span>{firstName+" "+lastName}</span>
-                    <br />  
-                    <span>{emailId}</span>
-                </div>
-            </NameTag>
-            <Box>
-                <textarea
-                 value={question}
-                 onChange={(e)=>setQuestion(e.target.value)}
-                 />
-            </Box>
-            <div style={{display:'flex' , justifyContent:"space-between"}}>
-                <span>
-                <span><input type="checkbox" /></span>
-                <span>Also post on my LinkedIn feed</span>
-                </span>
-                <span><Button onClick={handlePostQuestion}>Post</Button></span>
-            </div>
-        </CommentBox>
-        <div>
-            {
-                comments.length && comments.map((item:IComments)=> <QuestionNAnswer key={item?._id} {...item}/>
-                )
-            }
+          <ImageHolder>
+            {/* <img src="https://via.placeholder.com/168x160" alt="" /> */}
+            <AccountCircleIcon style={{ fontSize: "3em", color: "grey" }} />
+          </ImageHolder>
+          <div>
+            <span>{firstName + " " + lastName}</span>
+            <br />
+            <span>{emailId}</span>
+          </div>
+        </NameTag>
+        <Box>
+          <textarea
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+        </Box>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <span>
+            <span>
+              <input type="checkbox" />
+            </span>
+            <span>Also post on my LinkedIn feed</span>
+          </span>
+          <span>
+            <Button onClick={handlePostQuestion}>Post</Button>
+          </span>
         </div>
+      </CommentBox>
+      <div>
+        {comments.length > 0 &&
+          comments.map((item: IComments) => (
+            <QuestionNAnswer key={item?._id} {...item} />
+          ))}
+      </div>
     </section>
-    )
+  );
 }
-
